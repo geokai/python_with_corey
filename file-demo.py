a1b848a9ab2e1b1cc88f786c5437bb3c0058f9c6
George Kaimakis <geomatus@mac.com>
2016-05-10 17:56:20 +0100

    reading with seek

--- file-demo.py
+++ file-demo.py
@@ -2,11 +2,18 @@
 
 with open ('test.txt', 'r') as f:
 
-    for line in f:
-        print (line, end='')
+    size_to_read = 10
 
-#    f_contents = f.readline()
-#    print (f_contents, end='')
-#
-#    f_contents = f.readline()
-#    print (f_contents, end='')
+    f_contents = f.read(size_to_read)
+    print (f_contents, end='')
+
+    f.seek(0)
+
+    f_contents = f.read(size_to_read)
+    print (f_contents)
+
+
+#    while len (f_contents) > 0:
+#        print (f_contents, end='*')
+#        f_contents = f.read(size_to_read)
+
