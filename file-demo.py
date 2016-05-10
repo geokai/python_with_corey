c8da9dfe54fc240c08bf4d1b8525a8608e935c7d
George Kaimakis <geomatus@mac.com>
2016-05-10 17:59:22 +0100

    not writable

--- file-demo.py
+++ file-demo.py
@@ -1,19 +1,5 @@
 # File Objects
 
 with open ('test.txt', 'r') as f:
-
-    size_to_read = 10
-
-    f_contents = f.read(size_to_read)
-    print (f_contents, end='')
-
-    f.seek(0)
-
-    f_contents = f.read(size_to_read)
-    print (f_contents)
-
-
-#    while len (f_contents) > 0:
-#        print (f_contents, end='*')
-#        f_contents = f.read(size_to_read)
+    f.write ('Test')
 
