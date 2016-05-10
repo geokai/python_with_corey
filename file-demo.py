aa554a583b68a0d86306df97a53b9707a6f0a0a1
George Kaimakis <geomatus@mac.com>
2016-05-10 18:10:57 +0100

    copy from file to file

--- file-demo.py
+++ file-demo.py
@@ -1,7 +1,7 @@
 # File Objects
 
-with open ('test2.txt', 'w') as f:
-    f.write ('Test')
-    f.seek (0)
-    f.write ('R')
+with open ('test.txt', 'r') as rf:
+    with open ('test_copy.txt', 'w') as wf:
+        for line in rf:
+            wf.write (line)
 
