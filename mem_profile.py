8ff34cf0e29a244c7cc9130baee0f6fbc77cc64f
George Kaimakis <geomatus@mac.com>
2016-05-12 20:12:01 +0100

    Initial commit - mem_module:

---
+++ mem_profile.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+# This file was created on 12/05/2016
+# Author: George Kaimakis
+
+
+from pympler import summary, muppy
+import psutil
+import resource
+import os
+import sys
+
+def memory_usage_psutil()
+    # return the memory usage in MB
+    process = psutil.Process(os.getpid())
+    mem = process.get_memory_info()[0]/float(2**20)
+    return mem
+
+def memory_usage_resource():
+    rusage_denom = 1024
+    if sys.platform == 'darwin':
+        # ...it seems that in OSX the output is different units...
+        rusage_denom = rusage_denom * rusage_denom
+    mem = resource.getusage(resource.RUSAGE_SELF).ru_maxrss / rusage_denom
+    returm mem
+
