c3dd914dffca85910fd2957a997b829e50089705
George Kaimakis <geomatus@mac.com>
2016-05-12 14:41:16 +0100

    Uses list comprehension without generator:

--- generator.py
+++ generator.py
@@ -5,11 +5,15 @@
 
 # generator tutorial from Corey Schafer's Python Tutorial Series
 
-def square_numbers (nums):
-    for i in nums:
-        yield (i * i)
+#def square_numbers (nums):
+#    for i in nums:
+#        yield (i * i)
 
-my_nums = square_numbers ([1,2,3,4,5])
+#my_nums = square_numbers ([1,2,3,4,5])
+
+my_nums = [x * x for x in [1,2,3,4,5]]
+
+print (my_nums)
 
 print ()
 #    print (next(my_nums))  # [1, 4, 9, 16, 25]
