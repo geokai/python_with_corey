2367e4180bf4c07a8fc10162919689487ec77993
George Kaimakis <geomatus@mac.com>
2017-07-20 01:34:57 +0100

    Adds file f_string.py:

---
+++ f_string.py
@@ -0,0 +1,9 @@
+# f_strings usage:
+
+greeting = 'Hello'
+name = 'Corey'
+
+message = '{}, {} Welcome!'.format(greeting, name.upper())
+#message = f'{greeting}, {name.upper()}. Welcome!'
+
+print(message)
