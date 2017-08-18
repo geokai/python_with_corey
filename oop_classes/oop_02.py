7dfe40779c6cf9ce7510223ea53f98d57fd0f439
George Kaimakis <geomatus@mac.com>
2017-08-18 20:13:47 +0100

    methods reformatted, classmethods added, tests amended:
    Amends methods to accept 'self'.
    'init' email variable constructed as first initial and last name,
    all in lowercase. 'lower' & 'title' methods used on name vars directly.
    Adds 'init' doc-string.
    Adds classmethod to alter class var 'set_raise' and also for 'from_string'
    as an alternative init (currently pass).
    Various amends to tests.

--- oop_classes/oop_02.py
+++ oop_classes/oop_02.py
@@ -1,47 +1,67 @@
-# Python Object-Oriented Programming:
+"""A class to create an Employee object with class variables
+and class methods.
+"""
+
+# Python Object-Oriented Programming by Corey Schafer:
+# https://youtube.com/user/schafer5/playlist
+
+# This file was created on 14/08/17
+# Author: George Kaimakis - https://github.com/geokai
+
 
 
 class Employee:
-
+    
     num_of_emps = 0
-    raise_amount = 1.04
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
-        """create an emplayee object with the following variables"""
-        self.first = first
-        self.last = last
+        """create an emplayee object in terms of a first and last name,
+        a pay amount and an email address.
+        """
+        self.first = first.title()
+        self.last = last.title()
         self.pay = pay
-        self.email = first + '.' + last + '@company.com'
-
+        self.email = first[0].lower() + '.' + last.lower() + '@company.com'
+        # increments the class variable to keep track of number
+        # of Employee objects created:
         Employee.num_of_emps += 1
 
     def full_name(self):
         """returns a cat of 'first' & 'last' variables"""
-        return '{} {}'.format(emp_1.first, emp_1.last)
+        return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         """multiplies the instance 'pay' by a percentage"""
-        self.pay = int(self.pay * raise_amount)
+        self.pay = int(self.pay * self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(self, emp_str):
+        pass
 
 
-print(Employee.num_of_emps)
-
-emp_1 = Employee('George', 'Kaimakis', 50000)
+emp_1 = Employee('george', 'kaimakis', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
-# print(emp_1.pay)
-# emp_1.apply_raise()
-# print(emp_1.pay)
+print("–" * 25)
+print("Number of employees: " + str(Employee.num_of_emps))
+print("–" * 25)
 
-# print(Employee.__dict__)
-
-# Employee.raise_amount = 1.05
-# emp_1.raise_amount = 1.05
-
+print()
 # print(emp_1.__dict__)
+# print("emp 1 pay: " + str(emp_1.pay))
+# emp_1.apply_raise()
+# print(emp_1.__dict__)
+# print("emp 2 pay: " + str(emp_2.pay))
+# print()
 
-# print(Employee.raise_amount)
-# print(emp_1.raise_amount)
-# print(emp_2.raise_amount)
+# print()
+print("Employee raise amount: " + str(Employee.raise_amt) + "%")
+print("emp_1 raise amount: " + str(emp_1.raise_amt))
+print("emp_2 raise amount: " + str(emp_2.raise_amt))
+print()
 
-print(Employee.num_of_emps)
